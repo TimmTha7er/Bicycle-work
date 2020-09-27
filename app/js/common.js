e29b310dfeeb46bcafbb112d66fb5c0c9db84b84
TimmTha7er <66261170+TimmTha7er@users.noreply.github.com>
2020-09-27 16:19:21 +0300

    add section twitter and tiny-slider

--- app/js/common.js
+++ app/js/common.js
@@ -16,7 +16,37 @@
       return document.createElement(type);
     }
 
-    console.log('h1llo from gulp')
+    // ----------------------------------------------
+    //		section twitter slider
+    // ----------------------------------------------
+    // add nav dots
+    function addNavDots(itemClass, containerClass) {
+      const count = getAllEl(itemClass).length;
+      const container = getEl(containerClass);
 
+      for (let i = 0; i < count; i++) {
+        let dot = createEl('div');
+        dot.className = `${containerClass.slice(1)}__dot`;
+        container.appendChild(dot);
+      }
+    }
+
+    addNavDots('.slider-item', '.twitter-nav-container');
+
+    // add slider
+    const pricingSlider = tns({
+      container: '.twitter-slider',
+      items: 1,
+      slideBy: 'page',
+      speed: 400,
+      mouseDrag: true,
+      controls: false,
+      nav: true,
+      navPosition: 'bottom',
+      navContainer: '.twitter-nav-container',
+      // controlsContainer: '.pricing-controls-btns',
+      // prevButton: '.pricing-controls-btns__prev',
+      // nextButton: '.pricing-controls-btns__next',
+    });
   });
 })();
